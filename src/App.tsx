191cd1bf703e25bf7be3c236482ea18cae148689
GuzeevZhenya <shyst.evgeny@mail.ru>
2022-10-25 21:49:38 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,24 @@
-import React from "react";
-import { Button } from "./Button";
+import React, { useState } from "react";
+import { FullInput } from "./Components/FullInput";
 import "./App.css";
 
 function App() {
-  // const clickEvent = (event: string) => {
-  //   console.log("hi");
-  // };
+  let [message, setMessage] = useState([
+    { message: "message-1" },
+    { message: "message-2" },
+    { message: "message-3" },
+  ]);
 
-  const onClickHandler = (name: string) => {
-    console.log(name);
-  };
-
-  const foo1 = () => {
-    console.log("10200");
-  };
-  const foo2 = (price: number) => {
-    console.log(price);
-  };
-
-  const ButtonClick = (subscriber: string) => {
-    console.log(subscriber);
-  };
-  const ButtonRemove = (subscriber: string) => {
-    console.log(subscriber);
+  const addMessage = (title: string) => {
+    let newMessage = { message: title };
+    setMessage([...message, newMessage]);
   };
   return (
     <div className="App">
-      {/* <button onClick={(event) => clickEvent('hi')}>Click</button> */}
-      {/* <button onClick={(event: MouseEvent<HTMLButtonElement></HTMLButtonElement>)=>onClickHandler("Vasia")}>Click</button> */}
-      {/* <button onClick={() => onClickHandler("info")}>Button</button>
-      <button onClick={foo1}>Click1</button>
-      <button onClick={() => foo2(100200)}>Click1</button> */}
-      <Button name={"click"} callBack={() => ButtonClick("click")} />
-      <Button name={"remove"} callBack={() => ButtonRemove("remove")} />
+      <FullInput addMessage={addMessage} />
+      {message.map((item, index) => {
+        return <div key={index}>{item?.message}</div>;
+      })}
     </div>
   );
 }
