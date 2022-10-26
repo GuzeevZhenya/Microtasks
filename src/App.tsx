2113e2d406cfa150538d0beaa31eb0258069b37d
GuzeevZhenya <shyst.evgeny@mail.ru>
2022-10-26 13:18:19 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
-//1
-// import React, {useState} from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
+// import React, { useState } from "react";
+// import ReactDOM from "react-dom";
+// import "./index.css";
 
-// function UsersList() {
-//     const results = useState<void>(["Bob", "Alex", "Ann"])
-//     const users = results[0]
-//     const setUsers = results[1]
+// export const UsersList = () => {
+//   const results = useState<Array<string>>(["Bob", "Alex", "Ann"]);
+//   const users = results[0];
+//   const setUsers = results[1];
 
-//     return (
-//        <p>Тут будет список пользователей</p>
-//     )
-// }
+//   return <p>Тут будет список пользователей</p>;
+// };
 
-// ReactDOM.render(
-//     <UsersList/>, document.getElementById('root')
-// );
+// ReactDOM.render(<UsersList />, document.getElementById("root"));
 
 //2
 
@@ -23,7 +18,7 @@
 // import ReactDOM from "react-dom";
 // import "./index.css";
 
-// export const UsersList = () => {
+//  export const  UsersList =()=> {
 //   const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"]);
 
 //   return <p>Тут будет список пользователей!</p>;
@@ -126,43 +121,43 @@
 
 //10
 
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+// import React, { useState } from "react";
+// import ReactDOM from "react-dom";
+// import "./index.css";
 
-type UserType = {
-  id: number;
-  name: string;
-  age: number;
-};
+// type UserType = {
+//   id: number;
+//   name: string;
+//   age: number;
+// };
 
-function User(props: UserType) {
-  return (
-    <li>
-      User {props.name}: {props.age} y.o.
-    </li>
-  );
-}
+// function User(props: UserType) {
+//   return (
+//     <li>
+//       User {props.name}: {props.age} y.o.
+//     </li>
+//   );
+// }
 
-export const UsersList = () => {
-  const state = [
-    { id: 1, name: "Bob", age: 34 },
-    { id: 2, name: "Alex", age: 25 },
-    { id: 3, name: "Ann", age: 30 },
-    { id: 4, name: "John", age: 23 },
-  ];
-  const users = [
-    { id: 1, userName: "Bob", age: 34 },
-    { id: 2, userName: "Alex", age: 25 },
-    { id: 3, userName: "Ann", age: 30 },
-    { id: 4, userName: "John", age: 23 },
-  ];
+// export const UsersList = () => {
+//   const state = [
+//     { id: 1, name: "Bob", age: 34 },
+//     { id: 2, name: "Alex", age: 25 },
+//     { id: 3, name: "Ann", age: 30 },
+//     { id: 4, name: "John", age: 23 },
+//   ];
+//   const users = [
+//     { id: 1, userName: "Bob", age: 34 },
+//     { id: 2, userName: "Alex", age: 25 },
+//     { id: 3, userName: "Ann", age: 30 },
+//     { id: 4, userName: "John", age: 23 },
+//   ];
 
-  const [usersList, setUsersList] = useState<Array<UserType>>(XXX);
-  return (
-    <main>
-      <h5>User list:</h5>
-      <p>Тут будет список пользователей</p>
-    </main>
-  );
-};
+//   const [usersList, setUsersList] = useState<Array<UserType>>(XXX);
+//   return (
+//     <main>
+//       <h5>User list:</h5>
+//       <p>Тут будет список пользователей</p>
+//     </main>
+//   );
+// };
