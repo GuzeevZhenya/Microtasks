9e4fa0f1ea75efa752b076aaddde2a96bbe3e275
GuzeevZhenya <shyst.evgeny@mail.ru>
2023-02-21 13:58:12 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,68 @@
-import React, {useState} from 'react';
-import './App.css';
-import {Site} from "./components/Site";
+import React, { useState } from "react";
+import "./App.css";
 
+// 1. type UserPropsType = {
+//   name: string;
+//   description: string;
+// };
+// export const User: React.FC<UserPropsType> = (props) => {
+//   return (
+//     <div>
+//       <h1>Имя: {props.name}</h1>
+//       <div>Описание: {props.description}</div>
+//     </div>
+//   );
+// };
 
-function App() {
-    return (
-        <div>
-            <Site/>
-        </div>
-    );
-}
+// 2.
+// type PagePropsType = {
+//   age: number;
+//   name: string;
+//   avatarUrl: string;
+// };
+// export const Page: React.FC<PagePropsType> = (props) => {
+//   return <User a={props.age} n={props.name} />;
+// };
+// type UserPropsType = {
+//   a: number;
+//   n: string;
+// };
+// export const User: React.FC<UserPropsType> = (props) => {
+//   return (
+//     <div>
+//       name: {props.n}, age: {props.a}
+//     </div>
+//   );
+// };
 
+// export const App = () => {
+//   return <Page age={0} name={""} avatarUrl={""} />;
+// };
 
-export default App;
+// Что нужно написать вместо xxx и yyy?
+// Ответ дайте через пробел, например: blabla onClick(props)
+
+//3
+// type UserWalletType = {
+//     title: string
+//     amount: number
+// }
+// type UserWalletPropsType = {
+//     wallet: UserWalletType
+// }
+
+// export const UserWallet: React.FC<UserWalletPropsType> = (props) => {
+//     return <div>title: {props.wallet.title}, amount: {props.wallet.amount}</div>
+// }
+
+// export const UserMoney = () => {
+//     const wallets = [
+//         {title: 'bitcoin', amount: 1},
+//         {title: '$', amount: 100}
+//     ]
+
+//     return <div>
+//         <UserWallet wallet={xxx} />
+//         <UserWallet wallet={yyy} />
+//     </div>
+// }
