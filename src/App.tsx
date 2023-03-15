775652217728fc0923d3b7bd53b37af515266363
GuzeevZhenya <shyst.evgeny@mail.ru>
2023-03-15 15:21:54 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
 //1
-//!!! не <Array<string>>
+//!!! Array<string>
 
-// import React, { useState } from "react";
-// import ReactDOM from "react-dom";
-// import "./index.css";
+// import React, {useState} from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
 
-// export const UsersList = () => {
-//   const results = useState<Array<string>>(["Bob", "Alex", "Ann"]);
-//   const users = results[0];
-//   const setUsers = results[1];
+// function UsersList() {
+//     const results = useState<any>(["Bob", "Alex", "Ann"])
+//     const users = results[0]
+//     const setUsers = results[1]
 
-//   return <p>Тут будет список пользователей</p>;
-// };
+//     return (
+//        <p>Тут будет список пользователей</p>
+//     )
+// }
 
-// ReactDOM.render(<UsersList />, document.getElementById("root"));
+// ReactDOM.render(
+//     <UsersList/>, document.getElementById('root')
+// );
+
+// Какой тип правильнее указать вместо "any" при типизации стэйта?
 
 //2
 //!!! function
